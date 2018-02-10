bd769ee8fa8aa0bb1f6ffacf2cfef9c9bf5bd8c5
fourside <fourside@gmail.com>
2018-02-10 17:57:32 +0900

    add header and footer

--- src/js/controller/footerController.js
+++ src/js/controller/footerController.js
@@ -1,3 +1,4 @@
+import '../../css/style.css';
 
 export default class MainController {
   constructor() {
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import angular from 'angular';
 import MainController from './controller/mainController'
+import HeaderController from './controller/headerController'
+import FooterController from './controller/footerController'
 
 angular.module('app' ,[
 ])
@@ -7,5 +9,16 @@ angular.module('app' ,[
   template: require('../template/main.html'),
   controller: MainController,
   controllerAs: 'main'
-});
+})
+.component('myHeader', {
+  template: require('../template/header.html'),
+  controller: HeaderController,
+  controllerAs: 'header'
+})
+.component('myFooter', {
+  template: require('../template/footer.html'),
+  controller: FooterController,
+  controllerAs: 'footer'
+})
+;
 
