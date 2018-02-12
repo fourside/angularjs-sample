775362b18c60a6f78d8732ebecad6e256eebc877
fourside <fourside@gmail.com>
2018-02-12 11:10:59 +0900

    auth guard

--- src/js/app.config.js
+++ src/js/app.config.js
@@ -1,5 +1,6 @@
 import LoginController from './controller/loginController';
 import AboutController from './controller/aboutController';
+import ErrorController from './controller/errorController';
 
 export default function routing($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('');
@@ -13,6 +14,11 @@ export default function routing($locationProvider, $routeProvider) {
       template: require('../template/about.html'),
       controller: AboutController,
       controllerAs: 'about'
+    })
+    .when('/error', {
+      template: require('../template/error.html'),
+      controller: ErrorController,
+      controllerAs: 'error'
     })
     .otherwise({
       redirectTo: '/'
--- src/js/controller/mainController.js
+++ src/js/controller/mainController.js
@@ -1,3 +1,18 @@
 export default class MainController {
-  constructor() {}
+  constructor($scope, $location, authService) {
+    // auth guard
+    $scope.$watch(
+      function() {
+        return authService.authed();
+      },
+      function(newValue) {
+        if (!newValue) {
+          const path = $location.path();
+          if (path !== '/' || path !== '/error') {
+            $location.path('/error');
+          }
+        }
+      }
+    );
+  }
 }
